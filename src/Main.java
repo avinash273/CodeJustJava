dd2c0b49575ab46f3a122a36d84b6d66085c3f9f
avinash273 <savinash273@gmail.com>
2022-11-29 00:29:57 -0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import Leetcode.twoSum;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        twoSum twoSum = new twoSum();
+        System.out.println("Test code 28 Nov 2022 " + twoSum);
+    }
+}
