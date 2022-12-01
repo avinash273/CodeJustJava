053b1128c10cc1786354f161092dddab8db8454c
avinash273 <savinash273@gmail.com>
2022-12-01 02:06:13 -0600

    lengthOfLongestSubstringMain R22.12.1

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,25 @@
 import Leetcode.twoSumMain;
+import Leetcode.lengthOfLongestSubstringMain;
 
 public class Main {
 
     public static void main(String[] args) {
-        /*
-          Main function used to call all programs
-          -Avinash Shanker
+        /**
+         * Main function used to call all program
+         * -Avinash Shanker
          */
 
-        /*
-          https://leetcode.com/problems/two-sum/
+        /**
+         * https://leetcode.com/problems/two-sum/
          */
         twoSumMain twoSumVar = new twoSumMain();
         int[] result = twoSumVar.twoSum(new int[]{2, 7, 11, 15}, 13);
         System.out.println("twoSum [" + result[0] + "," + result[1] + "]");
+
+        /**
+         * https://leetcode.com/problems/longest-substring-without-repeating-characters/
+         */
+        lengthOfLongestSubstringMain lengthOfLongestSubstringVar = new lengthOfLongestSubstringMain();
+        System.out.println("lengthOfLongestSubstringMain: " + lengthOfLongestSubstringVar.lengthOfLongestSubstring("abcabcbb"));
     }
 }
