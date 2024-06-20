191ee81f0d7498eb137d096d65f721bffbb56d58
avinash273 <savinash273@gmail.com>
2024-06-19 22:59:32 -0500

    InheritanceExample

---
+++ src/Basics/InheritanceExample.java
@@ -0,0 +1,39 @@
+package Basics;
+
+class Calculator{
+    public int add(int a, int b){
+        return a + b;
+    }
+
+    public int subtract(int a, int b){
+        return a - b;
+    }
+
+}
+
+class AdvancedCalculator extends Calculator{
+    public int multiply(int a, int b){
+        return a * b;
+    }
+
+    public int divide(int a, int b){
+        return a / b;
+    }
+}
+
+class VeryAdvancedCalculator extends Calculator{
+    public double power(double a, double b){
+        return Math.pow(a, b);
+    }
+}
+
+public class InheritanceExample {
+    public static void main(String[] args) {
+        AdvancedCalculator calculator = new AdvancedCalculator();
+        VeryAdvancedCalculator veryCalculator = new VeryAdvancedCalculator();
+        System.out.println(calculator.multiply(2, 3));
+        System.out.println(calculator.add(2, 3));
+        System.out.println(calculator.divide(2, 1));
+        System.out.println(veryCalculator.add(2, 3));
+    }
+}
--- src/Basics/StringBufferExample.java
+++ src/Basics/StringBufferExample.java
@@ -1,10 +1,28 @@
 package Basics;
 
+class Adder{
+    int x;
+    int y;
+
+    public static int add(int x, int y){
+       int z = x + y;
+       return z;
+    }
+
+}
+
+
 public class StringBufferExample {
     public static void main(String[] args) {
         StringBuffer test = new StringBuffer();
         test.append("Avinash");
 
+        Adder adder = new Adder();
+        adder.x = 1;
+        adder.y = 2;
+
+
         System.out.println(test.append(" shanker").delete(0,5));
+        System.out.println(Adder.add(3,7));
     }
 }
